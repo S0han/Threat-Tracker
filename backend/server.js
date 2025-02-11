f67e8c38f7283da0d7859ce78f6b1f30c10eb054
S0han <kane.pereira18@gmail.com>
2025-02-11 11:03:34 -0500

    udpate: fetch data from endpointy and return

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,26 @@ const express  = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Setup Success!")
+});
+
+app.get('/api/threats', async (req, res) => {
+    const URL = "https://urlhaus-api.abuse.ch/v1/urls/recent/limit/10/";
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error(`Repsonse status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-})
+});
