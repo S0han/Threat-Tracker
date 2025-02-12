b8533e604760468469efdb33b5ef40f7f89cd131
S0han <kane.pereira18@gmail.com>
2025-02-12 08:12:37 -0500

    update: create dummy data to apply logic to threats table from instructions that mimics data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>React App</h1>
     </div>
   );
--- frontend/src/components/ThreatsTable.jsx
+++ frontend/src/components/ThreatsTable.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 export default function ThreatsTable() {
+    const dummy_threats = [
+        { id: 1, host: "cat.com", url: "https://cat.com", threatType: "catSteal", dateAdded: "2024-02-10" },
+        { id: 1, host: "dog.com", url: "https://dog.com", threatType: "dogAttack", dateAdded: "2024-02-10" }
+    ]
+    
     return (
-        <div></div>
+        <div>
+
+        </div>
     );
 }
