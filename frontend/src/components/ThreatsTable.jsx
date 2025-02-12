b9cf36b96530b8549d86514d4a374279aad1f469
S0han <kane.pereira18@gmail.com>
2025-02-12 10:55:22 -0500

    update: add filter logic for table based on threat type

--- frontend/src/components/ThreatsTable.jsx
+++ frontend/src/components/ThreatsTable.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ThreatsTable() {
+    const [filterThreat, setFilterThreat] = useState('');
+    
     const dummy_threats = [
         { id: 1, host: "cat.com", url: "https://cat.com", threatType: "catSteal", dateAdded: "2024-02-10" },
-        { id: 1, host: "dog.com", url: "https://dog.com", threatType: "dogAttack", dateAdded: "2024-02-10" }
+        { id: 2, host: "dog.com", url: "https://dog.com", threatType: "dogAttack", dateAdded: "2024-02-10" }
     ]
     
+    const uniqueThreatTypes = [...new Set(dummy_threats.map((item) => item.threatType))];
+    const filteredThreats = filterThreat 
+        ? dummy_threats.filter((item) => (item.threatType === filterThreat))
+        : dummy_threats;
+
     return (
         <div>
+            
+            <label>Filter by Threat Type</label>
+            <select onChange={(e) => setFilterThreat(e.target.value)} value={filterThreat}>
+                <option value="">All</option>
+                {
+                    uniqueThreatTypes.map((threat) => (
+                        <option key={threat} value={threat}>{threat}</option>
+                    ))
+                }
+            </select>
+
             <table>
                 <thead>
                     <tr>
@@ -19,7 +37,7 @@ export default function ThreatsTable() {
                 </thead>
                 <tbody>
                     {
-                        dummy_threats.map(item => (
+                        filteredThreats.map(item => (
                             <tr key={item.id}>
                                 <td>{item.host}</td>
                                 <td>{item.url}</td>
