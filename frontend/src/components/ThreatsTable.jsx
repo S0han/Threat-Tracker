a03c6c913b09e9d3efe8a2b6ebd96bc368685d8f
S0han <kane.pereira18@gmail.com>
2025-02-12 13:27:05 -0500

    update: add buttons with pagination logic to display pages fetched

--- frontend/src/components/ThreatsTable.jsx
+++ frontend/src/components/ThreatsTable.jsx
@@ -93,6 +93,17 @@ export default function ThreatsTable() {
                     }
                 </tbody>
             </table>
+
+            <div>
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>Previous</button>
+                <span> Page {page} </span>
+                <button 
+                    disabled={realThreats.length < limit}
+                    onClick={() => setPage(page + 1)}
+                >
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
