b29dfbc5c8d0b778bf57cc23c8e3c0d570ca011d
S0han <kane.pereira18@gmail.com>
2025-02-15 19:01:20 -0500

    update: modularize server.js and place endpoint logic in threats.js for fetching

--- backend/routes/threats.js
+++ backend/routes/threats.js
@@ -3,17 +3,15 @@ const prisma = require('../config/db');
 
 const router = express.Router();
 
-app.get('/api/threats', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 5;
-
         const threats = await prisma.threat.findMany({
             skip: (page - 1) * limit,
             take: limit,
             orderBy: { date_added: 'desc' }
         });
-
         res.json({ page, limit, threats });
     } catch (error) {
         console.error(error.message);
@@ -21,4 +19,50 @@ app.get('/api/threats', async (req, res) => {
     }
 });
 
-module.exports = router;
+router.get('/fetch', async (req, res) => {
+    const URL = "https://urlhaus-api.abuse.ch/v1/urls/recent/limit/10";
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        return date.toISOString();
+    };
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.query_status !== "ok") {
+            throw new Error("External API error");
+        }
+        const threat_list = data.urls.map(item => ({
+            host: item.host,
+            url: item.url,
+            threat_type: item.threat,
+            date_added: formatDate(item.date_added)
+        }));
+        await prisma.threat.createMany({
+            data: threat_list,
+            skipDuplicates: true
+        });
+        res.json({
+            message: "Threats fetched and stored successfully",
+            count: threat_list.length
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+const fetchThreatsOnce = async () => {
+    const URL = "http://localhost:3001/api/threats/fetch";
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data.message);
+    } catch (error) {
+        console.error('Error fetching threats:', error.message);
+    }
+};
+
+module.exports = { router, fetchThreatsOnce };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const prisma = require('./config/db');
 const authRoutes = require("./routes/auth");
-const threatRoutes = require("./routes/threats");
+const { router: threatRoutes, fetchThreatsOnce } = require("./routes/threats");
 
 const app = express();
 const PORT = 3001;
@@ -16,60 +15,6 @@ app.use('/api/threats', threatRoutes);
 app.get('/', (req, res) => {
     res.send("Main Page");
 });
-
-app.get('/api/fetch-threats', async (req, res) => {
-    const URL = "https://urlhaus-api.abuse.ch/v1/urls/recent/limit/10";
-
-    const formatDate = (dateStr) => {
-        const date = new Date(dateStr);
-        return date.toISOString();
-    };
-    
-    try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-            throw new Error(`Repsonse status: ${response.status}`);
-        };
-        
-        const data = await response.json();
-        if (data.query_status !== "ok") {
-            throw new Error("External API error")
-        };
-
-        const threat_list = data.urls.map(item => (
-            {
-                host: item.host,
-                url: item.url,
-                threat_type: item.threat,
-                date_added: formatDate(item.date_added)
-            }
-        ));
-
-        await prisma.threat.createMany({
-            data: threat_list,
-            skipDuplicates: true
-        });
-
-        res.json({ 
-            message: "Threats fetched and stored successfully", 
-            count: threat_list.length 
-        });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-
-const fetchThreatsOnce = async () => {
-    const URL = "http://localhost:3001/api/fetch-threats";
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        console.log(data.message);
-    } catch (error) {
-        console.error('Error fetching threats:', error.message);
-    }
-}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
